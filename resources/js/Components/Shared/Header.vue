56d88c6993365066a92a31a1d1fb832f988e1ca1
pascual214 <adrian.pascualblesa@gmail.com>
2026-03-04 01:26:06 +0100

    Pequeñas modificaciones en el header

--- resources/js/Components/Shared/Header.vue
+++ resources/js/Components/Shared/Header.vue
@@ -10,16 +10,29 @@
             </a>
         </div>
 
+        <div class="flex-none ml-15">
+            <ul class="menu menu-horizontal gap-3">
+                <li>
+                    <button class="btn btn-ghost font-bold">Home</button>
+                </li>
+                <li>
+                    <button class="btn btn-ghost">Pizzara</button>
+                </li>
+            </ul>
+
+            <!-- Implementación de más botones si fuese necesario (contacto, sobre nosotros...) -->
+        </div>
+
         <!-- Gracias a este div vacío el ancla de la imagen no ocupa toda la largura del div hasta los botones, solo la imágen  -->
         <div class="flex-1"></div>
 
         <div class="flex-none">
             <ul class="menu menu-horizontal p-0 gap-5">
                 <li>
-                    <button class="btn btn-primary">Iniciar sesión</button>
+                    <button class="btn btn-primary">Registrarse</button>
                 </li>
                 <li>
-                    <button class="btn btn-primary">Registrarse</button>
+                    <button class="btn btn-outline btn-primary">Iniciar sesión</button>
                 </li>
             </ul>
         </div>
